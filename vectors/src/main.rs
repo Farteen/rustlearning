0045188fae4f73c2d6edef3b653d586b43618574
farteen <xujavacn@gmail.com>
2017-10-18 10:08:07 +0800

    add vectors

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -5,10 +5,18 @@ fn main() {
 
     // let v = vec![1, 2, 3];
 
-    let mut v = Vec::new();
-    v.push(5);
-    v.push(6);
-    v.push(7);
-    v.push(8);
+    // let mut v = Vec::new();
+    // v.push(5);
+    // v.push(6);
+    // v.push(7);
+    // v.push(8);
+
+    // let v = vec![1, 2, 3, 4, 5];
+    // let third: &i32 = &v[2];
+    // let third: Option<&i32> = v.get(2);
+
+    let mut v = vec![1, 2, 3, 4, 5];
+    let first = &v[0];
+    v.push[6];
 
 }
---
+++ vectors/target/debug/deps/vectors-6959871da68e53d5.d
@@ -0,0 +1,5 @@
+/Users/glasses/Desktop/Move/Rustlang/vectors/target/debug/deps/vectors-6959871da68e53d5.rmeta: src/main.rs
+
+/Users/glasses/Desktop/Move/Rustlang/vectors/target/debug/deps/vectors-6959871da68e53d5.d: src/main.rs
+
+src/main.rs:
